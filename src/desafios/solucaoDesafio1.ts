60fe4c69ea31d7149490c4ea47f34da2c5889cba
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-07-01 19:37:02 -0300

    Resoluçao do desafio 1

---
+++ src/desafios/solucaoDesafio1.ts
@@ -0,0 +1,13 @@
+interface employee{
+    code: number,
+    name: string
+}
+
+const funcionario = {
+    code: 10,
+    name: 'John'
+}
+
+console.log(funcionario.code);
+console.log(funcionario.name);
+
